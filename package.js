69d12be57a3e23fca3da712652bb843db492e068
arichter83 <36486293+arichter83@users.noreply.github.com>
2018-10-08 12:48:19 +0200

    Change repo URL

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   summary: 'Reactstrap UI Components for accounts-React',
-  version: '0.5.0',
+  version: '0.5.1',
   name: 'meteoreact:accounts-reactstrap',
-  git: 'https://github.com/Biscoint/accounts-reactstrap.git',
+  git: 'https://github.com/understandr/accounts-reactstrap.git',
   documentation: null,
 });
 
